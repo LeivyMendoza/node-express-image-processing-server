e61c895c7439d255f87d4f2c761a8ee3a36e7a6f
Leivy Mendoza <mendozaleivy@gmail.com>
2023-01-29 12:25:22 -0500

    create the image upload page

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 const app = require('./app');
 
-module.exprts = app.listen(3000, () => {
+module.exports = app.listen(3000, () => {
   console.log(`App running on port: ${3000}`);
 });
+
+
+// const express = require('express');
+// const app = express(), bodyParser = require("body-parser"), port = 3080;
+
+// app.use(bodyParser.json());
+
+// app.get('/', (req, res) => {
+//     res.send('App Works !!!!');
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server listening on the port:${port}`);
+// });
+
