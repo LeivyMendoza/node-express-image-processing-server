24916360222acf38cf1ce23a5496cc04e41d20b5
Leivy Mendoza <mendozaleivy@gmail.com>
2023-01-29 15:06:56 -0500

    register the on message, error and exit event listener for the resize worker

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -10,6 +10,7 @@ module.exports = function imageProcessor(filename) {
     const sourcePath = uploadPathResolver(filename);
     const resizedDestination = uploadPathResolver('resized-' + filename);
     const monochromeDestination = uploadPathResolver('monochrome-' + filename);
+    let resizeWorkerFinished = false;
     return new Promise((resolve, reject) => {
         if(isMainThread) {
             try {
@@ -17,12 +18,23 @@ module.exports = function imageProcessor(filename) {
                     {workerData: {source: sourcePath, destination: resizedDestination}});
                 const monochromeWorker = new Worker(pathToMonochromeWorker, 
                     {workerData: {source: sourcePath, destination: monochromeDestination}});
+                resizeWorker.on('message', (message) => {
+                    resizeWorkerFinished = true;
+                    resolve('resizeWorker finished processing');
+                });
+                resizeWorker.on('error', (error) => {
+                    reject(new Error(error.message));
+                });
+                resizeWorker.on('exit', (code) => {
+                    if(code !== 0) {
+                        reject(new Error('Exited with status code ' + code));
+                    };
+                });
             } catch(error) {
                 reject(error);
             }
         } else {
             reject(new Error("not on main thread"));
         }
-        resolve();
     });
 }
