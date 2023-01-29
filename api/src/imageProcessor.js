ac408be3b406e6eb02b02939805bf6e60fd3e7de
Leivy Mendoza <mendozaleivy@gmail.com>
2023-01-29 14:32:14 -0500

    instantiate a new promise

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -2,5 +2,8 @@ const path = require('path');
 const {Worker, isMainThread} = require('worker_threads');
 
 module.exports = function imageProcessor() {
-    
+    return new Promise((resolve, reject) => {
+        if(!isMainThread) reject(new Error("not on main thread"));
+        resolve();
+    });
 }
